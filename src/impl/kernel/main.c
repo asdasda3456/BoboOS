09bb5994c39922b8e5191f8319a87c9fd97964ee
asdasda3456 <contact.minceraftyt@gmail.com>
2024-09-29 19:01:12 +0300

    Initial commit

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,21 @@
+#include "../../intf/print.h"
+
+void kernel_main() 
+{
+    print_clear();
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("AsdasdaOS x86_64 Kernel ");
+    newline();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("AsdasdaOS x86_64 Kernel ");
+    newline();
+    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
+    print_str("AsdasdaOS x86_64 Kernel ");
+    newline();
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+    print_str("AsdasdaOS x86_64 Kernel ");
+    newline();
+    print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_BLACK);
+    print_str("AsdasdaOS x86_64 Kernel ");
+    newline();
+}
